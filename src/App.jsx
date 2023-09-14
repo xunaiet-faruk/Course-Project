ad844af5ed6c4bd07d62430725b3b5d676f3826a
xunaiet <xunaiet28@gmail.com>
2023-09-14 19:38:32 +0600

    The header part

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <h1 className='text-4xl text-center font-bold mt-10'>Course Registration</h1>
+   
+    </>
+  )
+}
+
+export default App
